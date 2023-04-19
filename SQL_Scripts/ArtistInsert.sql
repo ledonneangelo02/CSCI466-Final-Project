47d3762313bc06726b59fff3f0cfb39d433b0215
Angelo Ledonne <z1920784@turing.cs.niu.edu>
2023-04-18 21:08:46 -0500

    Oopsy, Fixed the Artist Name field lul

--- SQL_Scripts/ArtistInsert.sql
+++ SQL_Scripts/ArtistInsert.sql
@@ -1,39 +1,46 @@
-##############################
-# This is a SQL Script that  #
-# will insert data into our  #
-# table.                     #
-##############################
-INSERT INTO Artist (FirstName) VALUES("Shawn Mendes");
-INSERT INTO Artist (FirstName) VALUES("Ariana Grande");
-INSERT INTO Artist (FirstName) VALUES("Ed Sheeran");
-INSERT INTO Artist (FirstName) VALUES("Post Malone");
-INSERT INTO Artist (FirstName) VALUES("Lil Tecca");
-INSERT INTO Artist (FirstName) VALUES("Sam Smith");
-INSERT INTO Artist (FirstName) VALUES("Lil Nas X");
-INSERT INTO Artist (FirstName) VALUES("Billie Eilish");
-INSERT INTO Artist (FirstName) VALUES("Lewis Capaldi");
-INSERT INTO Artist (FirstName) VALUES("Drake");
-INSERT INTO Artist (FirstName) VALUES("Chris Brown");
-INSERT INTO Artist (FirstName) VALUES("Y2K");
-INSERT INTO Artist (FirstName) VALUES("Lizzo");
-INSERT INTO Artist (FirstName) VALUES("MEDUZA");
-INSERT INTO Artist (FirstName) VALUES("Tones and I");
-INSERT INTO Artist (FirstName) VALUES("Young Thug");
-INSERT INTO Artist (FirstName) VALUES("Katy Perry");
-INSERT INTO Artist (FirstName) VALUES("Martin Garrix");
-INSERT INTO Artist (FirstName) VALUES("Kygo");
-INSERT INTO Artist (FirstName) VALUES("Taylor Swift");
-INSERT INTO Artist (FirstName) VALUES("Lady Gaga");
-INSERT INTO Artist (FirstName) VALUES("Marshmello");
-INSERT INTO Artist (FirstName) VALUES("The Chainsmokers");
-INSERT INTO Artist (FirstName) VALUES("Social House");
-INSERT INTO Artist (FirstName) VALUES("Khalid");
-INSERT INTO Artist (FirstName) VALUES("Justin Bieber");
-INSERT INTO Artist (FirstName) VALUES("Rick Ross");
-INSERT INTO Artist (FirstName) VALUES("Macklemore");
-INSERT INTO Artist (FirstName) VALUES("J. Cole");
-INSERT INTO Artist (FirstName) VALUES("Travis Scott");
-INSERT INTO Artist (FirstName) VALUES("Patrick Stump");
-INSERT INTO Artist (FirstName) VALUES("Chance The Rapper");
-INSERT INTO Artist (FirstName) VALUES("PnB Rock");
-INSERT INTO Artist (FirstName) VALUES("Kane Brown");
+##########################################
+# CSCI 466  FINAL GROUP PROJECT  SEC: 02 #
+#					 #
+#  This is a SQL Script that will fill   #
+#     our Artist Table with data.        #
+#					 #
+#    By:    Angelo LeDonne z1920784      #
+#           Chris Dejong   z1836870      #
+#           Mark Southwood z058227       #
+#           Milad Jizan    z1943173      #
+##########################################
+
+INSERT INTO Artist (Name) VALUES("Shawn Mendes");
+INSERT INTO Artist (Name) VALUES("Ariana Grande");
+INSERT INTO Artist (Name) VALUES("Ed Sheeran");
+INSERT INTO Artist (Name) VALUES("Post Malone");
+INSERT INTO Artist (Name) VALUES("Lil Tecca");
+INSERT INTO Artist (Name) VALUES("Sam Smith");
+INSERT INTO Artist (Name) VALUES("Lil Nas X");
+INSERT INTO Artist (Name) VALUES("Billie Eilish");
+INSERT INTO Artist (Name) VALUES("Lewis Capaldi");
+INSERT INTO Artist (Name) VALUES("Drake");
+INSERT INTO Artist (Name) VALUES("Chris Brown");
+INSERT INTO Artist (Name) VALUES("Y2K");
+INSERT INTO Artist (Name) VALUES("Lizzo");
+INSERT INTO Artist (Name) VALUES("MEDUZA");
+INSERT INTO Artist (Name) VALUES("Tones and I");
+INSERT INTO Artist (Name) VALUES("Young Thug");
+INSERT INTO Artist (Name) VALUES("Katy Perry");
+INSERT INTO Artist (Name) VALUES("Martin Garrix");
+INSERT INTO Artist (Name) VALUES("Kygo");
+INSERT INTO Artist (Name) VALUES("Taylor Swift");
+INSERT INTO Artist (Name) VALUES("Lady Gaga");
+INSERT INTO Artist (Name) VALUES("Marshmello");
+INSERT INTO Artist (Name) VALUES("The Chainsmokers");
+INSERT INTO Artist (Name) VALUES("Social House");
+INSERT INTO Artist (Name) VALUES("Khalid");
+INSERT INTO Artist (Name) VALUES("Justin Bieber");
+INSERT INTO Artist (Name) VALUES("Rick Ross");
+INSERT INTO Artist (Name) VALUES("Macklemore");
+INSERT INTO Artist (Name) VALUES("J. Cole");
+INSERT INTO Artist (Name) VALUES("Travis Scott");
+INSERT INTO Artist (Name) VALUES("Patrick Stump");
+INSERT INTO Artist (Name) VALUES("Chance The Rapper");
+INSERT INTO Artist (Name) VALUES("PnB Rock");
+INSERT INTO Artist (Name) VALUES("Kane Brown");
