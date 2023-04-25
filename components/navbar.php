1f9420538fc2f3cad78f6679837d4b6f54bf8cb0
Christopher Dejong <z1836870@turing.cs.niu.edu>
2023-04-25 17:14:17 -0500

    Navbar fix

--- components/navbar.php
+++ components/navbar.php
@@ -1,6 +1,4 @@
-
-
-<nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top">
+<nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top container-fluid">
 	<div class="container-md">
 		<a class="navbar-brand" href="./">
 			<img src="assets/path50.svg" alt="DJ Logo" height="40" style="filter:invert(100%);">
@@ -10,7 +8,7 @@
 			aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
-		<div class="collapse navbar-collapse" id="navbarNav">
+		<div class="collapse navbar-collapse container-fluid clearfix" id="navbarNav">
 			<ul class="navbar-nav">
 				<li class="nav-item">
 				<a class="nav-link active" aria-current="page" href="./">Home</a>
@@ -18,11 +16,16 @@
 				<li class="nav-item">
 				<a class="nav-link active" aria-current="page" href="./user.php">User</a>
 				</li>
+				<li class="nav-item">
+				<a class="nav-link active" aria-current="page" href="./dj.php">DJ</a>
+				</li>
 			</ul>
-			<form class="d-flex my-auto" action="" method="POST">
-      			<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      			<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    		</form>
+			<div class="container-fluid clearfix">
+			<form class="d-flex my-auto float-end" action="" method="POST">
+				<input class="form-control mr-sm-2 me-2" type="search" placeholder="Search" aria-label="Search">
+				<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+			</form>
+			</div>
 		</div>
 	</div>
 </nav>
