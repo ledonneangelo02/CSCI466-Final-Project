4f46c2d357462aa1eb27199418b5a5958150f2fe
Christopher Dejong <z1836870@turing.cs.niu.edu>
2023-04-26 21:07:04 -0500

    That pause button works now lmfao

--- components/activeplayer.php
+++ components/activeplayer.php
@@ -52,20 +52,24 @@
 					<script>
 						var i = 0;
 						var x = <?php echo $activeLength; ?>;
+						var s = true;
 
 						var interval = setInterval( increment, 100);
 						function increment(){
-							document.getElementById("ActivePlayer").style.width = ((i/x) * 100) + "%";
-							var dateA = new Date(0);
-							var dateB = new Date(0);
-							dateA.setSeconds(i); // specify value for SECONDS here
-							dateB.setSeconds(x); // specify value for SECONDS here
-							var timeStringA = dateA.toISOString().substring(14, 19);
-							var timeStringB = dateB.toISOString().substring(14, 19);
-							document.getElementById('ActiveTime').innerText = timeStringA + " / " + timeStringB;
-							if (i >= x)
-								clearInterval(interval);
-							i += .1;
+							if (s == true)
+							{
+								document.getElementById("ActivePlayer").style.width = ((i/x) * 100) + "%";
+								var dateA = new Date(0);
+								var dateB = new Date(0);
+								dateA.setSeconds(i); // specify value for SECONDS here
+								dateB.setSeconds(x); // specify value for SECONDS here
+								var timeStringA = dateA.toISOString().substring(14, 19);
+								var timeStringB = dateB.toISOString().substring(14, 19);
+								document.getElementById('ActiveTime').innerText = timeStringA + " / " + timeStringB;
+								if (i >= x)
+									clearInterval(interval);
+								i += .1;
+							}
 						}
 					</script>
 					<div>
@@ -75,9 +79,14 @@
 						<div id="ActivePlayer" class="progress-bar" style="width: 0%"></div>
 					</div><br/>
 					<button type="button" class="btn btn-light"><i class="bi bi-rewind"></i></button>
-					<button type="button" class="btn btn-light"><i class="bi bi-pause-btn"></i></button>
+					<button id="ActivePause" type="button" class="btn btn-light"><i class="bi bi-pause-btn"></i></button>
 					<button type="button" class="btn btn-light"><i class="bi bi-fast-forward"></i></button>
 					</div><br/>
+					<script>
+						document.getElementById("ActivePause").addEventListener('click',function () {
+							s = !s;
+						});
+					</script>
 					<?php
 						foreach($artistVocalized as $ActiveArtist)
 						{
