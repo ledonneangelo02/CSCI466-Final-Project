54611041fd7e5a8523b01e8497b26920eeeea069
Mark Southwood <z058227@hopper.cs.niu.edu>
2023-05-01 16:34:00 -0500

    Fuck this shit

--- components/navbar.php
+++ components/navbar.php
@@ -25,9 +25,9 @@
 			</ul>
 			<ul class="navbar-nav me-auto">
 				<li class="nav-item">
-					<form class="d-flex my-auto" action="" method="POST">
+					<form class="d-flex my-auto" action="" method="GET">
 						<input class="form-control mr-sm-2 me-2" type="search" name ="search" placeholder="Search" aria-label="Search">
-						<button class="btn btn-outline-success my-2 my-sm-0" type="submit" name="navsearch" value="navsearch">Search</button>
+						<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
 					</form>
 				</li>
 			</ul>
