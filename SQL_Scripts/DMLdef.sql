6538695faaa55020e4f8c19c72c070eb664556d6
Angelo Ledonne <z1920784@turing.cs.niu.edu>
2023-05-01 22:03:07 -0500

    Split the DDL and DML just in case they search my cell, i have began to love it here. There are three meals a day and the voices seem the quietest when im in these walls. How can I ever live as a free man knowing I will fall back right into my JavaScript addiction? I have destroied countless lives with my extremely cheap Bootcamps, not knowing I was spreading such an evil addiction. It made my wife cheat on me with her boss... AND IT CAN GET YOU A WIFE TOO! COME ON DOWN TO ANGELOS JAVASCRIPT AND AJAX BOOTCAMP, WHERE WE CAN GET YOU ON A PATH TO BE JUST LIKE ME

---
+++ SQL_Scripts/DMLdef.sql
@@ -0,0 +1,15 @@
+########################
+# Please Just Run      #
+# DBdef.sql, it has all#
+# the goodies you need #
+########################
+
+\. SongInsert.sql
+\. ArtistInsert.sql
+\. PersonInsert.sql
+\. RoleInsert.sql
+\. KFInsert.sql
+\. AssociatedWithInsert.sql
+\. ContributesInsert.sql
+\. EnqueuesInsert.sql
+
