f5116f4959333dcafa8294b5f3df1acd4e894571
Milad Jizan <z1943173@turing.cs.niu.edu>
2023-05-02 14:42:15 -0500

    Fixed the error that were caused by the github merge.

--- signUp.php
+++ signUp.php
@@ -66,26 +66,16 @@
                 if (isset($_POST['submit']))
                 {
                     //Check if any form elements are missing and print an error.
-<<<<<<< HEAD
                     if ($_POST['Email'] == null)
                     {
                         echo "You did not enter an email address! \n";
 		    }
                     else if ($_POST['FirstName'] == null)
-=======
-   
-		    if ($_POST['Email'] == null)
-		    {
-			echo "You did not enter an email! \n";
-		    }
-	            else if ($_POST['FirstName'] == null)
->>>>>>> origin
                     {
                         echo "You did not enter your first name! \n";
                     }
                     else
 		    {
-<<<<<<< HEAD
 			$key = $_POST['Email'];
 			$kill = false;
 			//Check if email alredy exists in the database and print an error if it does.
@@ -108,9 +98,6 @@
 
 
                         $sql = 'INSERT INTO Person (FirstName, LastName, Email, AddressLine1, AddressLine2) VALUES (:FirstName, :LastName, :Email, :AddressLine1, :AddressLine2);';
-=======
-			$sql = 'INSERT INTO Person (FirstName, LastName, Email, AddressLine1, AddressLine2) VALUES (:FirstName, :LastName, :Email, :AddressLine1, :AddressLine2);';
->>>>>>> origin
                         $stmt = $PDO->prepare($sql);
                         $stmt->execute(['FirstName' => $_POST['FirstName'], 'LastName' => $_POST['LastName'], 'Email' => $_POST['Email'], 'AddressLine1' => $_POST['AddressLine1'], 'AddressLine2' => $_POST['AddressLine2']]);
                         
@@ -129,7 +116,7 @@
                         $vals = $statement->fetchAll(PDO::FETCH_ASSOC);
                         $row = $vals[0];
 
-			echo "<h4> User: " . $row["FirstName"] . " " . $row["LastName"] . " created!</h4>";
+                        echo "<h4> User: " . $row["FirstName"] . " " . $row["LastName"] . " created!</h4>";
                     }
                 
                 }
